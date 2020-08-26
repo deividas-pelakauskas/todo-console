6b54f1cced53cb53f4c8ba1ab4499956232c82c4
Deividas Pelakauskas <pelakauskas1@gmail.com>
2020-08-26 22:48:51 +0100

    Console menu added

--- todo.py
+++ todo.py
@@ -3,8 +3,36 @@ Author: Deividas Pelakauskas
 
 """
 
+
 class Task:
 
     def __init__(self, name, deadline):
         self.name = name
-        self. deadline = deadline
+        self.deadline = deadline
+
+
+def main():
+    menu_status = True
+    while menu_status:
+        option = input("1. View current tasks\n"
+                       "2. Add new task\n"
+                       "3. Delete task\n"
+                       "0. Exit\n")
+
+        if option == "1":
+            pass
+
+        elif option == "2":
+            pass
+
+        elif option == "3":
+            pass
+
+        elif option == "0":
+            menu_status = False
+
+        else:
+            print("Unrecognised option")
+
+
+main()
