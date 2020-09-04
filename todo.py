72a6d0d8621ecc2e880ff8d8b0495349e037e04b
Deividas Pelakauskas <pelakauskas1@gmail.com>
2020-09-04 20:05:34 +0100

    Add task function

--- todo.py
+++ todo.py
@@ -3,15 +3,34 @@ Author: Deividas Pelakauskas
 
 """
 
+import datetime
 
 class Task:
 
-    def __init__(self, name, deadline):
+    def __init__(self, name, deadline, completed):
         self.name = name
         self.deadline = deadline
+        self.completed = completed
 
+def enter_date():
+    """
+    Enter date with validation
+
+    :return: date in format of DD/MM/YYYY
+    """
+    while True:
+        task_deadline = input("Enter date in following format - DD/MM/YYYY:\n")
+        try:
+            datetime.datetime.strptime(task_deadline, '%d/%m/%Y')
+            return task_deadline
+        except ValueError:
+            print("Incorrect data format, should be DD/MM/YYYY")
 
 def main():
+    tasks = []
+    tasks.append(Task("Task 1", "10/09/2020", False))
+    tasks.append(Task("Task 2", "15/10/2021", False))
+
     menu_status = True
     while menu_status:
         option = input("1. View current tasks\n"
@@ -20,10 +39,20 @@ def main():
                        "0. Exit\n")
 
         if option == "1":
-            pass
+            if len(tasks) > 0:
+                print("\n")
+                print("DEADLINE\tTASK")
+                for task in tasks:
+                    print(task.deadline + "\t" + task.name)
+                print("\n")
+            else:
+                print("Task list is empty")
 
         elif option == "2":
-            pass
+            task_name = input("Enter task name:\n")
+            task_deadline = enter_date()
+            tasks.append(Task(task_name, task_deadline, False))
+            print("Successfully added new task")
 
         elif option == "3":
             pass
